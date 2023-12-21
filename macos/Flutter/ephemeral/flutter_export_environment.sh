a4d1e66d344f4d89d3ebb6f2ba815f51d4b3d29a
Sabbir Hasan <sabbirdev07@gmail.com>
2023-12-21 11:41:53 +0600

    replace all

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\flutter_windows_3.10.6-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\iOSApp\sau_directory_app"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\WOW\Desktop\Flutter\sau_directory_app-typesafe\sau_directory_app-typesafe"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NAME=2.0.0"
+export "FLUTTER_BUILD_NUMBER=22"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
